a5dd551a432038d92c1519570666f030d59a8110
AutoSDE <autosde@il.ibm.com>
2020-04-20 15:15:28 +0300

    Integration of generated code
    Fixed specs to work with new code
    Added generated code to git
    added vcrs to code

--- spec/models/manageiq/providers/autosde/autosde_client_spec.rb
+++ spec/models/manageiq/providers/autosde/autosde_client_spec.rb
@@ -9,29 +9,28 @@ describe ManageIQ::Providers::Autosde::PhysicalInfraManager::AutosdeClient do
   it "raises on login with wrong credentials" do
     client = ManageIQ::Providers::Autosde::PhysicalInfraManager::AutosdeClient.new('asfd', :host => '9.151.190.224')
     VCR.use_cassette("incorrect_login_spec") do
-      expect{client.login}.to raise_error(Exception, ManageIQ::Providers::Autosde::PhysicalInfraManager::AutosdeClient::AUTH_ERRR_MSG)
+      expect { client.login }.to raise_error(Exception, ManageIQ::Providers::Autosde::PhysicalInfraManager::AutosdeClient::AUTH_ERRR_MSG)
     end
   end
 
   it "gets a list of storage systems" do
     client = ManageIQ::Providers::Autosde::PhysicalInfraManager::AutosdeClient.new(:host => '9.151.190.224')
-    VCR.use_cassette("correct_login_spec") do
-      client.login
-    end
+    temp = {}
 
     VCR.use_cassette("get_storage_systems") do
-      expected = [{"management_ip" => "9.151.156.155",
-                   "name" => "my_xiv",
-                   "storage_array" => nil,
-                   "storage_family" => "",
-                   "system_type" =>
-                       {"name" => "a_line",
-                        "short_version" => "4",
-                        "uuid" => "c87d3686-6a36-4358-bc8d-7028d24d60d3",
-                        "version" => "4"},
-                   "uuid" => "f09afcc9-53be-4573-8b6f-1e5787d34d05"}]
-      expect(client.get_storage_systems).to eq(expected)
+      temp[:systems] = client.storage_system_api.storage_systems_get
     end
+
+    systems = temp[:systems]
+    expected = {:management_ip => "9.151.156.155", :name => "my_xiv", :storage_family => "",
+                 :system_type => {
+                     :name => "a_line", :short_version => "4", :uuid => "c87d3686-6a36-4358-bc8d-7028d24d60d3",
+                     :version => "4"}, :uuid => "f09afcc9-53be-4573-8b6f-1e5787d34d05"}
+
+    expect(systems.count).to eq 1
+    expect(systems.first).to be_instance_of OpenapiClient::StorageSystem
+    expect(systems.first.to_hash).to eq expected
+
   end
 
 end
--- spec/models/manageiq/providers/autosde/physical_infra_manager_spec.rb
+++ spec/models/manageiq/providers/autosde/physical_infra_manager_spec.rb
@@ -6,13 +6,14 @@ describe ManageIQ::Providers::Autosde::PhysicalInfraManager do
 
   it "has credentials and hostname" do
 
+    ems = FactoryBot.create(:autosde_manager, :with_authentication, :name => "kaka")
     expect(ManageIQ::Providers::Autosde::PhysicalInfraManager.all[0].name).to eq 'kaka'
     expect(ManageIQ::Providers::PhysicalInfraManager.all[0].authentication_userid).to eq "testuser"
     expect(ManageIQ::Providers::PhysicalInfraManager.all[0].address).to eq "ems-0000000000001"
   end
 
   it "has autosde client" do
-    ems = FactoryBot.create(:autosde_manager, :with_authentication, :name => "kaka")
+    ems = FactoryBot.create(:autosde_manager, :with_authentication, )
     expect(ManageIQ::Providers::PhysicalInfraManager.all[0].autosde_client).to be_instance_of ManageIQ::Providers::Autosde::PhysicalInfraManager::AutosdeClient
   end
 
