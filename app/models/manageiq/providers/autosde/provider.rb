22057a506fbf6164214b903f4ac439ea89946c83
gregoryb <gregoryb@il.ibm.com>
2020-05-14 06:20:06 +0300

    SDE-1173: WIP -- Enabled creation and listing, but for sde, not storage.
    Add Autosde provider class, and bound it to Autosde::SdeManager.
    Added missing items to EmsSdeController: table_name, ems_path, new_ems_path, restful?.
    Created show_dashboard HAMLs for sde_controller and pointed them to ems_physical_infra dashboard. This is wrong, but there isn't an sde dashboard.
    Created report configuration YAML for sde-manager, and pointed it to SdeManager.

---
+++ app/models/manageiq/providers/autosde/provider.rb
@@ -0,0 +1,8 @@
+class ManageIQ::Providers::Autosde::Provider < ::Provider
+  has_one :ems_sde_manager,
+          :foreign_key => "provider_id",
+          :class_name  => "ManageIQ::Providers::Autosde::SdeManager",
+          :autosave    => true
+
+  validates :name, :presence => true, :uniqueness => true
+end
