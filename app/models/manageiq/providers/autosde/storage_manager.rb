f2ab2f41596ab2574cdbd10e3bc0d775da319df2
Ilya Kessel <ilyak@il.ibm.com>
2020-06-03 12:43:56 +0300

    1191 added autosde block storage properties

--- app/models/manageiq/providers/autosde/storage_manager.rb
+++ app/models/manageiq/providers/autosde/storage_manager.rb
@@ -151,12 +151,12 @@ class ManageIQ::Providers::Autosde::StorageManager < ManageIQ::Providers::Storag
     n_('Storage Manager', 'Storage Managers', number)
   end
 
-
-
-
-
-
-
+  def textual_group_list
+    [
+        %i[properties status],
+        %i[storage_relationships topology]
+    ]
+  end
 
   def autosde_client
     if @autosde_client.nil?
