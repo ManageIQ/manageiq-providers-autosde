f80f95a14e18db875d0e5c1d3dfb6388b87f79d0
GREGORYBEYER <gregoryb@il.ibm.com>
2020-06-09 11:29:37 +0300

    SDE-1222: Moved autosde volume-creation from storage-bolume to cloud-volume.
    Changed the cloud-volume creation for task to sync for easier development. Need to undo this before release.

--- app/models/manageiq/providers/autosde/storage_manager/cloud_volume.rb
+++ app/models/manageiq/providers/autosde/storage_manager/cloud_volume.rb
@@ -1,21 +1,24 @@
 class ManageIQ::Providers::Autosde::StorageManager::CloudVolume < ::CloudVolume
   supports :create
 
+  # Has to be override the base method.. It's supposed to implement creating the volume on the EMS.
+  # But I only create it in MIQ DB for now.
   # @param [ExtManagementSystem] _ext_management_system
   def self.raw_create_volume(_ext_management_system, _options = {})
-    storage_resource = StorageResource.find(_options[:storage_resource_id])
-
-    puts "--------------------------------------------------------------------------------------"
-    puts "------------------------------------------raw_create_volume----------------------------------"
-    puts ext_management_system.inspect
-    puts storage_resource.inspect
+    storage_resource = _options[:storage_resource]
     # todo [per gregoryb]: here we need to send the command to autosde to create the volume
 
     self.create(
         :ext_management_system => _ext_management_system,
         :storage_resource => storage_resource,
-        **options
+        **_options
     )
 
+  end
+
+  # has to be overriden and return a specifically-formatted hash.
+  def self.validate_create_volume(ext_management_system)
+    # check that the ems isn't nil and return a correctly formatted hash.
+    validate_volume(ext_management_system)
   end
 end
