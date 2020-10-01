3f62159923ac04a1dcfd36a2d7283e0951428784
Erez Theodorou <erezt@il.ibm.com>
2020-10-01 22:04:57 +0300

    AutoSDE provider Related Tables

---
+++ app/models/manageiq/providers/autosde/storage_manager/cloud_volume.rb
@@ -0,0 +1,51 @@
+class ManageIQ::Providers::Autosde::StorageManager::CloudVolume < ::CloudVolume
+  supports :create
+  supports :safe_delete
+
+  # Has to be override the base method.. It's supposed to implement creating the volume on the EMS.
+  # But I only create it in MIQ DB for now.
+  # @param [ManageIQ::Providers::Autosde] _ext_management_system
+  def self.raw_create_volume(_ext_management_system, _options = {})
+    # @type [StorageService]
+    storage_service = _options[:storage_service]
+
+    vol_to_create = _ext_management_system.autosde_client.VolumeCreate(
+      :service => storage_service.ems_ref,
+      :name    => _options[:name],
+      :size    => _options[:size]
+    )
+    _ext_management_system.autosde_client.VolumeApi.volumes_post(vol_to_create)
+    EmsRefresh.queue_refresh(_ext_management_system)
+  end
+
+  # has to be overriden and return a specifically-formatted hash.
+  def self.validate_create_volume(ext_management_system)
+    # check that the ems isn't nil and return a correctly formatted hash.
+    validate_volume(ext_management_system)
+  end
+
+  # ================= delete  ================
+
+  def raw_delete_volume
+    ems = ext_management_system
+    ems.autosde_client.VolumeApi.volumes_pk_delete(ems_ref)
+    EmsRefresh.queue_refresh(ems)
+  end
+
+  def validate_delete_volume
+    {:available => true, :message => nil}
+  end
+
+  # ================ safe-delete ================
+
+  def validate_safe_delete_volume
+    {:available => true, :message => nil}
+  end
+
+  def raw_safe_delete_volume
+    ext_management_system.autosde_client.VolumeApi.volumes_safe_delete(ems_ref)
+    # self.ext_management_system.autosde_client.VolumeSafeDelete.vo(self.ems_ref)
+
+    EmsRefresh.queue_refresh(ext_management_system)
+  end
+end
---
+++ app/models/manageiq/providers/autosde/storage_manager/physical_storage.rb
@@ -0,0 +1,23 @@
+class ManageIQ::Providers::Autosde::StorageManager::PhysicalStorage < ::PhysicalStorage
+  supports :create
+
+  # @param [ManageIQ::Providers::Autosde] _ext_management_system
+  def self.raw_create_physical_storage(_ext_management_system, _options = {})
+    sys_to_create = _ext_management_system.autosde_client.StorageSystemCreate(
+      :name           => _options[:name],
+      :password       => _options[:password],
+      :user           => _options[:user],
+      :system_type    => _options[:system_type],
+      :auto_add_pools => true,
+      :auto_setup     => true,
+      :management_ip  => _options[:management_ip],
+      :storage_family => "ontap_7mode"
+    )
+
+    begin
+      _ext_management_system.autosde_client.StorageSystemApi.storage_systems_post(sys_to_create)
+    ensure
+      EmsRefresh.queue_refresh(_ext_management_system)
+    end
+  end
+end
