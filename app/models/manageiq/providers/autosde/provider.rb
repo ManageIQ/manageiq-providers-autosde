5691d18a0790a6387e1f30d2c33f5b23c72860e5
Gregory Beyer <gregoryb@il.ibm.com>
2020-10-04 16:46:06 +0300

    fixes #8 removed unused AutosdeProvider class

--- app/models/manageiq/providers/autosde/provider.rb
+++
@@ -1,8 +0,0 @@
-class ManageIQ::Providers::Autosde::Provider < ::Provider
-  has_one :ems_sde_manager,
-          :foreign_key => "provider_id",
-          :class_name  => "ManageIQ::Providers::Autosde::StorageManager",
-          :autosave    => true
-
-  validates :name, :presence => true, :uniqueness => true
-end
