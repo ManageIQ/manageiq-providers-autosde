7b5fd37f73b650b5d6d520d2ea9a38f09ca0fa06
Gregory Beyer <gregoryb@il.ibm.com>
2020-10-05 15:26:36 +0300

    Added decorator for the EMS

---
+++ app/decorators/manageiq/providers/autosde/storage_manager_decorator.rb
@@ -0,0 +1,32 @@
+module ManageIQ::Providers
+  class Autosde::StorageManagerDecorator < MiqDecorator
+    def self.fonticon
+      'pficon pficon-server'
+    end
+
+    def fileicon
+      "svg/vendor-#{image_name}.svg"
+    end
+
+    def quadicon
+      icon = {
+          :top_left     => {
+              :text    => t = cloud_volumes.size,
+              :tooltip => n_("%{number} Cloud Volume", "%{number} Cloud Volumes", t) % {:number => t}
+          },
+          :top_right    => {
+              :text    => t = storage_resources.size,
+              :tooltip => n_("%{number} Storage Pool", "%{number} Storage Pools", t) % {:number => t}
+          },
+          :bottom_left  => {
+              :fileicon => fileicon,
+              :tooltip  => ui_lookup(:model => type)
+          },
+          :bottom_right => QuadiconHelper.provider_status(authentication_status, enabled?)
+      }
+
+      icon[:middle] = QuadiconHelper::POLICY_SHIELD if get_policies.present?
+      icon
+    end
+  end
+end
