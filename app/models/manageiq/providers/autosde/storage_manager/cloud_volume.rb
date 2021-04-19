a08e0e172064282a43645e2b1c1a20f0a9272223
Adam Grare <adam@grare.com>
2021-04-19 14:56:34 -0400

    Add CloudVolume supports update

--- app/models/manageiq/providers/autosde/storage_manager/cloud_volume.rb
+++ app/models/manageiq/providers/autosde/storage_manager/cloud_volume.rb
@@ -1,6 +1,9 @@
 class ManageIQ::Providers::Autosde::StorageManager::CloudVolume < ::CloudVolume
   supports :create
   supports :safe_delete
+  supports :update do
+    unsupported_reason_add(:update, _("The Volume is not connected to an active Provider")) unless ext_management_system
+  end
 
   def self.raw_create_volume(_ext_management_system, _options = {})
     # @type [StorageService]
