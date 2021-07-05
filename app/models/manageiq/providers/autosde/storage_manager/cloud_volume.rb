50f0510a8b431f331bd41a3ee24d1508a608d585
IlyaKessel <58298268+IlyaKessel@users.noreply.github.com>
2021-07-05 09:26:24 +0300

    review changes

--- app/models/manageiq/providers/autosde/storage_manager/cloud_volume.rb
+++ app/models/manageiq/providers/autosde/storage_manager/cloud_volume.rb
@@ -5,12 +5,12 @@ class ManageIQ::Providers::Autosde::StorageManager::CloudVolume < ::CloudVolume
     unsupported_reason_add(:update, _("The Volume is not connected to an active Provider")) unless ext_management_system
   end
 
-  def self.raw_create_volume(_ext_management_system, _options = {})
+  def self.raw_create_volume(_ext_management_system, options = {})
     # @type [StorageService]
     vol_to_create = _ext_management_system.autosde_client.VolumeCreate(
-      :service => _options["storage_service"],
-      :name    => _options["name"],
-      :size    => _options["size"]
+      :service => options["storage_service"],
+      :name    => options["name"],
+      :size    => options["size"]
     )
     _ext_management_system.autosde_client.VolumeApi.volumes_post(vol_to_create)
     EmsRefresh.queue_refresh(_ext_management_system)
