1b07e9c3e059c41ed815069666009c21aadd5c66
Adam Grare <adam@grare.com>
2021-11-30 12:11:46 -0500

    Don't need to enable only present inventory_collections

--- app/models/manageiq/providers/autosde/inventory/persister/storage_manager.rb
+++ app/models/manageiq/providers/autosde/inventory/persister/storage_manager.rb
@@ -6,67 +6,15 @@ class ManageIQ::Providers::Autosde::Inventory::Persister::StorageManager < Manag
   attr_reader  :collections
 
   def initialize_inventory_collections
-    add_san_addresses
-    add_host_initiators
-    add_volume_mappings
-    add_wwpn_candidates
-    add_host_initiator_groups
-    add_physical_storages
-    add_physical_storage_families
-    add_storage_resources
-    add_storage_services
-    add_cloud_volumes
-  end
-
-  protected
-
-  # san addresses
-  def add_san_addresses
     add_collection(storage, :san_addresses)
-  end
-
-  # host initiators
-  def add_host_initiators
     add_collection(storage, :host_initiators)
-  end
-
-  # volume mappings
-  def add_volume_mappings
     add_collection(storage, :volume_mappings)
-  end
-
-  # wwpn candidates
-  def add_wwpn_candidates
     add_collection(storage, :wwpn_candidates)
-  end
-
-  # host-initiator-groups (cluster)
-  def add_host_initiator_groups
     add_collection(storage, :host_initiator_groups)
-  end
-
-  # physical storages
-  def add_physical_storages
     add_collection(storage, :physical_storages)
-  end
-
-  # physical storage families
-  def add_physical_storage_families
     add_collection(storage, :physical_storage_families)
-  end
-
-  # storage resources
-  def add_storage_resources
     add_collection(storage, :storage_resources)
-  end
-
-  # storage services
-  def add_storage_services
     add_collection(storage, :storage_services)
-  end
-
-  # cloud volumes
-  def add_cloud_volumes
     add_collection(storage, :cloud_volumes)
   end
 end
