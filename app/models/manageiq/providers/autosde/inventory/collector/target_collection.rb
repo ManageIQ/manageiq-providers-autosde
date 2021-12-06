4037e8eb3aa2c97554285a8daf1a038fd21e70de
Adam Grare <adam@grare.com>
2021-12-06 14:15:01 -0500

    Move add_target and references to core collector

--- app/models/manageiq/providers/autosde/inventory/collector/target_collection.rb
+++ app/models/manageiq/providers/autosde/inventory/collector/target_collection.rb
@@ -66,21 +66,10 @@ class ManageIQ::Providers::Autosde::Inventory::Collector::TargetCollection < Man
     target.targets.each do |target|
       case target
       when PhysicalStorage
-        add_target(:physical_storages, target.ems_ref)
+        add_target!(:physical_storages, target.ems_ref)
       when CloudVolume
-        add_target(:cloud_volumes, target.ems_ref)
+        add_target!(:cloud_volumes, target.ems_ref)
       end
     end
-  end
-
-  def add_target(association, ems_ref)
-    return if ems_ref.blank?
-
-    target.add_target(:association => association, :manager_ref => {:ems_ref => ems_ref})
-  end
-
-  # This helps us reference all unique references by collection e.g. all VM targets
-  def references(collection)
-    target.manager_refs_by_association&.dig(collection, :ems_ref)&.to_a&.compact || []
   end
 end
