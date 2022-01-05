121802140b4851824e473043cf0a67c648bfa07a
GREGORYBEYER <Gregory.Beyer@kyndryl.com>
2022-01-05 17:25:59 +0200

    Improving UI representation of host-initiator-groups and volume-mappings

--- app/models/manageiq/providers/autosde/storage_manager/host_volume_mapping.rb
+++ app/models/manageiq/providers/autosde/storage_manager/host_volume_mapping.rb
@@ -1,3 +1,6 @@
 class ManageIQ::Providers::Autosde::StorageManager::HostVolumeMapping <
   ManageIQ::Providers::Autosde::StorageManager::VolumeMapping
+  def mapped_to
+    host_initiator
+  end
 end
