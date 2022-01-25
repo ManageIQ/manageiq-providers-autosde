e70b354ae897da8a66bbab763120da477e0609ae
Adam Grare <adam@grare.com>
2022-01-25 14:43:15 -0500

    Override display name for volume_mappings in subclasses

--- app/models/manageiq/providers/autosde/storage_manager/host_volume_mapping.rb
+++ app/models/manageiq/providers/autosde/storage_manager/host_volume_mapping.rb
@@ -1,6 +1,5 @@
-class ManageIQ::Providers::Autosde::StorageManager::HostVolumeMapping <
-  ManageIQ::Providers::Autosde::StorageManager::VolumeMapping
-  def mapped_to
-    host_initiator
+class ManageIQ::Providers::Autosde::StorageManager::HostVolumeMapping < ManageIQ::Providers::Autosde::StorageManager::VolumeMapping
+  def name
+    _("Volume <%{cloud_volume_name}> is mapped to Host Initiator <%{host_initiator_name}>") % {:cloud_volume_name => cloud_volume.name, :host_initiator_name => host_initiator.name}
   end
 end
