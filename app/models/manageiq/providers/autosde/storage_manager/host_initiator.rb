ffaad3a9ba0982cce87e5dd77d2ec9e9c7706d01
Oferlis <Ofer.Lis@ocean.ibm.com>
2022-08-07 12:06:35 +0300

    Added support for multiple IQNs, parsing the array and passing it

--- app/models/manageiq/providers/autosde/storage_manager/host_initiator.rb
+++ app/models/manageiq/providers/autosde/storage_manager/host_initiator.rb
@@ -5,15 +5,17 @@ class ManageIQ::Providers::Autosde::StorageManager::HostInitiator < ::HostInitia
   def self.raw_create_host_initiator(ext_management_system, options = {})
     wwpns = Array(options['custom_wwpn'])
     wwpns += Array(options['wwpn']).map { |item| item["value"] }
-
-    # wwpn values send to autosde should be format as colon separated string (e.g. WWPN1:WWPN2:WWPN3)
     wwpn_values = wwpns.join(":")
+    # wwpn values send to autosde should be format as colon separated string (e.g. WWPN1:WWPN2:WWPN3)
+
+    iqns = Array(options['iqn'])
+    iqn_values = iqns.join(":")
 
     host_initiator_to_create = ext_management_system.autosde_client.StorageHostCreate(
       :name           => options['name'],
       :port_type      => options['port_type'],
       :storage_system => PhysicalStorage.find(options['physical_storage_id']).ems_ref,
-      :iqn            => options['iqn'] || "",
+      :iqn            => iqn_values || "",
       :wwpn           => wwpn_values || "",
       :chap_name      => options['chap_name'] || "",
       :chap_secret    => options['chap_secret'] || ""
