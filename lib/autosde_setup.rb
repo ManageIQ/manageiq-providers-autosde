c9fded492975259fefaaf31ee669cd0da4468855
eilam <eilam.elimelech@kyndryl.com>
2022-08-14 13:18:33 +0300

    fix

--- lib/autosde_setup.rb
+++ lib/autosde_setup.rb
@@ -9,14 +9,14 @@ class AutosdeSetup
     vol_dialog = YAML.load_file("plugins/manageiq-providers-autosde/content/miq/dialogs/volume.yaml")
     catalog_name = vol_dialog["label"]
     exist_dialog = Dialog.find_by(:label => catalog_name)
-    if exist_dialog
-      vol_dialog_id = exist_dialog.id
-    else
-      vol_dialog_id = DialogImportService.new.import(vol_dialog).id
-    end
+    vol_dialog_id = if exist_dialog
+                      exist_dialog.id
+                    else
+                      DialogImportService.new.import(vol_dialog).id
+                    end
     # ITEM
     vol_item_prop = { :name => "Volume", :prov_type => "autosde", :display => "true", :service_template_catalog_id => catalog_id,
-                      :config_info => { :provision => { :fqname => "/Storage/AutoSde/ServiceVolumeRequestApproval/Default", :dialog_id => vol_dialog_id } } }
+                      :config_info => { :provision => { :fqname => "/Storage/AutoSde/ServiceVolumeRequestApproval/Default", :dialog_id => vol_dialog_id }}}
     ServiceTemplate.create_catalog_item(vol_item_prop, nil) unless ServiceTemplate.find_by(:name => vol_item_prop[:name]) != nil
   end
 end
