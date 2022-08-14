980933bf6d085ef5f40def9782cf49cff53bd01e
eilam <eilam.elimelech@kyndryl.com>
2022-08-14 10:46:31 +0300

    fix

--- app/models/manageiq/providers/autosde/storage_manager/physical_storage.rb
+++ app/models/manageiq/providers/autosde/storage_manager/physical_storage.rb
@@ -45,17 +45,10 @@ class ManageIQ::Providers::Autosde::StorageManager::PhysicalStorage < ::Physical
     )
 
     begin
+      require File.join(__dir__,"plugins/manageiq-providers-autosde/tools/autosde_setup")
+      AutosdeSetup.service_catalog_components
+
       new_storage = ext_management_system.autosde_client.StorageSystemApi.storage_systems_post(sys_to_create)
-      ### SETUP SERVICE CATALOG COMPONENTS ###
-      # CATALOG
-      catalog_id = ServiceTemplateCatalog.find_or_create_by(:name => "AutoSDE Catalog").id
-      # DIALOG
-      vol_dialog = YAML.load_file('plugins/manageiq-providers-autosde/service_catalog/volume/dialog.yaml')
-      vol_dialog_id = DialogImportService.new.import(vol_dialog).id
-      # ITEM
-      vol_item_prop = { :name => "Volume", :prov_type => "autosde", :display => "true", :service_template_catalog_id => catalog_id,
-                        :config_info => { :provision => { :fqname => "/Storage/AutoSde/Services/ServiceVolumeRequestApproval/Default", :dialog_id => vol_dialog_id } } }
-      ServiceTemplate.create_catalog_item(vol_item_prop, nil) unless ServiceTemplate.find_by(:name => vol_item_prop[:name]) != nil
     ensure
       EmsRefresh.queue_refresh(
         InventoryRefresh::Target.new(
---
+++ tools/autosde_setup.rb
@@ -0,0 +1,22 @@
+class AutosdeSetup
+  def self.service_catalog_components
+    ### SETUP SERVICE CATALOG COMPONENTS ###
+    # CATALOG
+    catalog_name = "AutoSDE Catalog"
+    catalog_id = ServiceTemplateCatalog.find_or_create_by(:name => catalog_name).id
+
+    # DIALOG
+    vol_dialog = YAML.load_file( File.join(__dir__,"plugins/manageiq-providers-autosde/content/miq/dialogs/volume.yaml"))
+    catalog_name = vol_dialog["label"]
+    exist_dialog = Dialog.find_by(:label => catalog_name)
+    if exist_dialog
+      vol_dialog_id = exist_dialog.id
+    else
+      vol_dialog_id = DialogImportService.new.import(vol_dialog).id
+    end
+    # ITEM
+    vol_item_prop = { :name => "Volume", :prov_type => "autosde", :display => "true", :service_template_catalog_id => catalog_id,
+                      :config_info => { :provision => { :fqname => "/Storage/AutoSde/ServiceVolumeRequestApproval/Default", :dialog_id => vol_dialog_id } } }
+    ServiceTemplate.create_catalog_item(vol_item_prop, nil) unless ServiceTemplate.find_by(:name => vol_item_prop[:name]) != nil
+  end
+end
