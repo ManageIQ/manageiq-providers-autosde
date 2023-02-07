6381478ae8b8ed3de8a8ad20bd67b046ff8e166c
Oferlis <Ofer.Lis@ocean.ibm.com>
2023-02-07 12:34:05 +0200

    fix lint errors

--- app/models/manageiq/providers/autosde/storage_manager/refresher.rb
+++ app/models/manageiq/providers/autosde/storage_manager/refresher.rb
@@ -1,5 +1,4 @@
 class ManageIQ::Providers::Autosde::StorageManager::Refresher < ManageIQ::Providers::BaseManager::Refresher
-
   def post_process_refresh_classes
     [ManageIQ::Providers::Autosde::StorageManager]
   end
